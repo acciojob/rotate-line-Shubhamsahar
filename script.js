7322048a1f8b498aadf92fc1398c8ae55afb2add
Shubhamsahar <kumarshubham765484@gmail.com>
2025-08-03 12:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-//your JS code here. If required.
 let angle = 0;
 const line = document.getElementById('line');
 
 setInterval(() => {
     angle += 2;
-    line.style.transform = `rotate(${angle}deg)`;
+    line.style.transform = `translate(-100px, 0) rotate(${angle}deg)`;
 }, 20);
 
